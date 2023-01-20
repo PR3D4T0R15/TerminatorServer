7ed80530409cb02683b91718d3698c34abf079f1
Matt <32712980+PR3D4T0R15@users.noreply.github.com>
2023-01-20 01:56:08 +0100

    Klasy zapytan do serwera

--- TerminatorServer/app.py
+++ TerminatorServer/app.py
@@ -1,15 +1,40 @@
 from flask import Flask
 from flask_restful import Api, Resource
 import pymongo
+import hashlib
 
 app = Flask(__name__) #create app flask
 api = Api(app) #create restfulapi
 
-class Terminator(Resource):
-    def get(self):
-        return {"data":"Hello World"}
+class login(Resource):
 
-api.add_resource(Terminator, "/terminator")
+    def get(self, name, test):
+        return {"name":name, "test":test}
+
+    def post(self):
+        return {"data":"Posted"}
+
+api.add_resource(login, "/login/<string:name>/<int:test>")
+
+class data(Resource):
+
+    def get(self, name, test):
+        return {"name":name, "test":test}
+
+    def post(self):
+        return {"data":"Posted"}
+
+api.add_resource(data, "/data/<string:name>/<int:test>")
+
+class dataMove(Resource):
+
+    def get(self, name, test):
+        return {"name":name, "test":test}
+
+    def post(self):
+        return {"data":"Posted"}
+
+api.add_resource(dataMove, "/datamove/<string:name>/<int:test>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- TerminatorServer/app_test.py
+++ TerminatorServer/app_test.py
@@ -2,6 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.get(BASE + "/terminator")
+response = requests.get(BASE + "/terminator" + "/matt/19")
 
 print(response.json())
