839ddf870f13224cc706536c67207bb2ca706e4f
Matt <32712980+PR3D4T0R15@users.noreply.github.com>
2023-01-20 01:01:16 +0100

    dodane pakiety

--- TerminatorServer/app.py
+++ TerminatorServer/app.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import pymongo
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
