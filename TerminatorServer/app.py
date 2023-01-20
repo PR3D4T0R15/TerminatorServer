864d06f36217ef69d59b9b20e98b6b75fbb65096
Matt <32712980+PR3D4T0R15@users.noreply.github.com>
2023-01-20 01:24:46 +0100

    Przykładowe zapytanie API i tester

--- TerminatorServer/app.py
+++ TerminatorServer/app.py
@@ -2,11 +2,17 @@ from flask import Flask
 from flask_restful import Api, Resource
 import pymongo
 
-app = Flask(__name__)
+app = Flask(__name__) #create app flask
+api = Api(app) #create restfulapi
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class Terminator(Resource):
+    def get(self):
+        return {"data":"Hello World"}
+
+api.add_resource(Terminator, "/terminator")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
