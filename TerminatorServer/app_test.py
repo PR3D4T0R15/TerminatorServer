724dc416f99e5ddc136a4be780d98fd389f44d2c
Matt <32712980+PR3D4T0R15@users.noreply.github.com>
2023-01-21 02:44:14 +0100

    Czyszczenie

--- TerminatorServer/app_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:8080"
-headers = { "Content-Type" : "application/json", "PASS":"mateusz", "LOGIN":"Mateusz", "newPASS":"jakub", "newLOGIN":"Jakub", "LISTNAME":"Do zrobienia"}
-
-response = requests.get(BASE + "/data", headers=headers)
-
-print(response.json())
